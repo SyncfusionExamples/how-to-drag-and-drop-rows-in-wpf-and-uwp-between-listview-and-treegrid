763f17310bdb32e86c0e94e3e8f373035bcc92f5
ROOT\gowtham.gopalsamy <gowtham.gopalsamy@syncfusion.com>
2019-01-10 15:27:52 +0530

    how-to-drag-and-drop-rows-in-wpf-and-uwp-between-listview-and-treegrid

---
+++ UWP/DragAndDrop_ListViewAndTreeGrid/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Syncfusion.UI.Xaml.TreeGrid;
+using Syncfusion.UI.Xaml.TreeGrid.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RowDragAndDrop_Demo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.TreeGrid.RowDragDropController = new TreeGridRowDragDropControllerExt(this.TreeGrid);
+            listView.CanDragItems = true;
+            listView.DragItemsStarting += ListView_DragItemsStarting;
+            listView.DragItemsCompleted += ListView_DragItemsCompleted;
+        }
+
+        private void ListView_DragItemsCompleted(ListViewBase sender, DragItemsCompletedEventArgs args)
+        {
+            foreach (var item in args.Items)
+            {
+                (listView.ItemsSource as ObservableCollection<PersonInfo>).Remove(item as PersonInfo);
+            }
+        }
+
+        private void ListView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            e.Data.Properties.Add("DraggedItem", listView.SelectedItem);
+            e.Data.SetText(StandardDataFormats.Text);
+        }
+    }
+}
---
+++ UWP/DragAndDrop_ListViewAndTreeGrid/Model.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RowDragAndDrop_Demo
+{
+    class PersonInfo
+    {
+        Random random = new Random(123123);
+        #region Private Fields
+
+        private static int _globalId = 0;
+        private int _id;
+        private string _firstName;
+        private string _lastName;
+        private bool _available;
+
+        private DateTime _birthdate;
+        private double _salary;
+        private ObservableCollection<PersonInfo> _children;
+
+        #endregion Private Fields
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the children.
+        /// </summary>
+        /// <value>The children.</value>
+        public ObservableCollection<PersonInfo> Children
+        {
+            get
+            {
+                return _children;
+            }
+            set
+            {
+                _children = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>The first name.</value>
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>The last name.</value>
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        [Display(AutoGenerateField = false)]
+        public bool Availability
+        {
+            get
+            {
+                return _available;
+            }
+            set
+            {
+                _available = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the DOB.
+        /// </summary>
+        /// <value>The DOB.</value>
+        public DateTime BirthDate
+        {
+            get
+            {
+                return _birthdate;
+            }
+            set
+            {
+                _birthdate = value; ;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the Salary.
+        /// </summary>
+        /// <value>The Salary.</value>
+        public double Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                _salary = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonInfo"/> class.
+        /// </summary>
+        public PersonInfo()
+            : this("Enter FirstName", "Enter LastName", 20000, new DateTime(2008, 10, 26), null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonInfo"/> class.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="salary">The salary.</param>
+        /// <param name="dob">The dob.</param>
+        /// <param name="maxGenerations">The max generations.</param>
+        public PersonInfo(string firstName, string lastName, double salary, DateTime dob, ObservableCollection<PersonInfo> child)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Salary = salary;
+            Availability = true;
+            BirthDate = dob;
+            Id = _globalId++;
+            Children = child;
+        }
+
+        #endregion Constructors
+    }
+}
